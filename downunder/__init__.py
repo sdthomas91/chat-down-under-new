2663c8997e075f3a7356d21ebcb6ae1bee2f8dfa
Samuel Thomas <samueldthomas91@gmail.com>
2024-02-27 14:38:10 +0000

    amended __init__ file to include a flask CLI command workaround for db table creation in psql. Common issue I have faced during other prjects with a reliable workaround. Also had to amend the DB_URL in env.py to ensure it connected to the appropriate psql port

--- downunder/__init__.py
+++ downunder/__init__.py
@@ -12,3 +12,13 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 db = SQLAlchemy(app)
 
 from downunder import routes #noqa
+
+
+#Flask CLI workaround for db creation - Python3 command not 
+# working as per my previous project - code found here
+# https://stackoverflow.com/questions/
+# 77477706/how-can-i-create-database-file-with-flask
+@app.cli.command("create_db")
+def create_db():
+    db.create_all()
+    print("Database tables created")
--- downunder/routes.py
+++ downunder/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from downunder import app,db
-
+from downunder.models import Topic, Question
 
 @app.route("/")
 def home():
