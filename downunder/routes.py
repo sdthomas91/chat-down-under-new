85788877dbc1ba0351a65cf0ae0d1812bd172c17
Samuel Thomas <samueldthomas91@gmail.com>
2024-02-28 12:11:23 +0000

    adds signup form template - used bootstrap and enchanced with fields and password confirmation. Added route to navigate to signup and render template

--- downunder/routes.py
+++ downunder/routes.py
@@ -4,4 +4,8 @@ from downunder.models import Topic, Question
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", page_title="Welcome to Chat Down Under")
+
+@app.route("/sign_up")
+def sign_up():
+    return render_template("sign_up.html", page_title="Sign Up!")
