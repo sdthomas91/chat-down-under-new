2755abd88127187d3a29b2daf9d764b91898896c
Samuel Thomas <samueldthomas91@gmail.com>
2024-03-07 16:05:30 +0000

    ammends __init__

--- downunder/__init__.py
+++ downunder/__init__.py
@@ -1,9 +1,9 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 if os.path.exists("env.py"):
     import env
-from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
@@ -11,7 +11,7 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  # local
 else:
-    uri = os.environ.get("DATABASE_URL")
+    uri = os.environ.get("DB_URL")
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
